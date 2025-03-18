ac3deda542b07151fb53c571c06eeb9e9020eaed
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-18 14:41:17 -0300

    Agregando Contenedores Principales

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,18 @@ const App = () => {
     <>
       <div className="bg-stone-950 text-white min-h-screen">
         <header className="border-b border-black/2 dark:border-white/30">
-          <h1 className="text-center text-2xl p-5">Calculadora de Propinas</h1>
-          
+          <h1 className="text-center text-2xl p-5">Calculadora de Propinas y Consumo</h1>
         </header>
+
+        <main className='max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
+          <div>
+            <h3>Menu</h3> 
+          </div>
+
+          <div>
+            <h3>Consumo</h3>        
+          </div>
+        </main>
       </div>
     </>
   )
