89f05426dbcf63e44658e4b7988d03485fb0a22a
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-18 17:11:14 -0300

    Agregando elementos al state de order

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -6,8 +6,13 @@ export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]);
     
     const additem = (item: MenuItem) => {
-        console.log(item);
+
+        const newItem = {...item, quantity: 1}
+        setOrder([...order, newItem])
     }
+
+    console.log(order);
+    
 
     return {
         additem
