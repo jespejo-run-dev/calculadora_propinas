1f23ddfc92945035b1512810b2efc7e5fa88f0b3
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-03-18 14:34:41 -0300

    db.ts y types de productos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import  {menuItems} from './data/db'
 
 const App = () => {
+
+  console.log(menuItems);
+  
+
   return (
     <>
       <div className="bg-stone-950 text-white min-h-screen">
         <header className="border-b border-black/2 dark:border-white/30">
           <h1 className="text-center text-2xl p-5">Calculadora de Propinas</h1>
+          
         </header>
       </div>
     </>
---
+++ src/data/db.ts
@@ -0,0 +1,97 @@
+import type { MenuItem } from "../types";
+
+export const menuItems: MenuItem[] = [
+    // 🥩 Comida
+    {
+      "id": 1,
+      "name": "Tabla de Quesos y Jamón Serrano",
+      "price": 12000
+    },
+    {
+      "id": 2,
+      "name": "Papas Bravas",
+      "price": 6000
+    },
+    {
+      "id": 3,
+      "name": "Choripán",
+      "price": 5000
+    },
+    {
+      "id": 4,
+      "name": "Alitas BBQ",
+      "price": 8500
+    },
+    {
+      "id": 5,
+      "name": "Hamburguesa Clásica",
+      "price": 9000
+    },
+    {
+      "id": 6,
+      "name": "Empanadas de Queso",
+      "price": 5500
+    },
+    {
+      "id": 7,
+      "name": "Nuggets de Pollo",
+      "price": 7000
+    },
+
+    // 🍹 Tragos
+    {
+      "id": 8,
+      "name": "Whisky (single shot)",
+      "price": 7000
+    },
+    {
+      "id": 9,
+      "name": "Pisco Sour",
+      "price": 6000
+    },
+    {
+      "id": 10,
+      "name": "Mojito",
+      "price": 6500
+    },
+    {
+      "id": 11,
+      "name": "Negroni",
+      "price": 7500
+    },
+    {
+      "id": 12,
+      "name": "Gin Tonic",
+      "price": 6500
+    },
+    {
+      "id": 13,
+      "name": "Cerveza Artesanal",
+      "price": 5000
+    },
+    {
+      "id": 14,
+      "name": "Tequila Shot",
+      "price": 7000
+    },
+    {
+      "id": 15,
+      "name": "Caipirinha",
+      "price": 6800
+    },
+    {
+      "id": 16,
+      "name": "Margarita",
+      "price": 7000
+    },
+    {
+      "id": 17,
+      "name": "Cuba Libre",
+      "price": 6500
+    },
+    {
+      "id": 18,
+      "name": "Aperol Spritz",
+      "price": 7200
+    }
+];
